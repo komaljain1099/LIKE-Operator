36bb822694b9248bab3840fb984ab545e3a9caa9
hermione <134724405+komaljain1099@users.noreply.github.com>
2024-02-23 23:45:38 +0530

    Add files via upload

---
+++ LIKE operator.sql
@@ -0,0 +1,25 @@
+SELECT customername, country from customers;
+
+
+SELECT customerName, country from customers
+where country LIKE "AUS%";
+
+SELECT customerName, country from customers
+where country LIKE "%S%";
+
+SELECT customerName, country from customers
+where country LIKE "U%";
+
+SELECT customerName, country from customers
+where country LIKE "U_";
+
+
+SELECT customerName, country from customers
+where country LIKE "%A";
+
+select customernumber, customername, state, country, creditLimit
+from customers;
+
+
+
+
